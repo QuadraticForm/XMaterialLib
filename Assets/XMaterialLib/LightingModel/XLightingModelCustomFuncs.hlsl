665834de667011ffeb5d77418a66e784e8203aa1
XuXing <xux660@hotmail.com>
2024-07-05 16:12:36 +0800

    progress

--- Assets/XMaterialLib/LightingModel/XLightingModelCustomFuncs.hlsl
+++ Assets/XMaterialLib/LightingModel/XLightingModelCustomFuncs.hlsl
@@ -1,35 +1,5 @@
-#ifndef CUSTOM_LIGHTING
-#define CUSTOM_LIGHTING
-
-void MainLight_float(float3 worldPos, out float3 direction, out float3 color, out float shadowAtten)
-{
-#ifdef SHADERGRAPH_PREVIEW
-    direction = normalize(float3(-0.5,0.5,-0.5));
-    color = float3(1,1,1);
-    shadowAtten = 1;
-#else
-    float4 shadowCoord = TransformWorldToShadowCoord(worldPos);
-    Light mainLight = GetMainLight(shadowCoord);
-    direction = mainLight.direction;
-    color = mainLight.color;
-    shadowAtten = mainLight.shadowAttenuation;
-#endif
-}
-
-void MainLight_half(half3 worldPos, out half3 direction, out half3 color, out half shadowAtten)
-{
-#ifdef SHADERGRAPH_PREVIEW
-    direction = normalize(half3(-0.5,0.5,0.5));
-    color = half3(1,1,1);
-    shadowAtten = 1;
-#else
-    half4 shadowCoord = TransformWorldToShadowCoord(worldPos);
-    Light mainLight = GetMainLight(shadowCoord);
-    direction = mainLight.direction;
-    color = mainLight.color;
-    shadowAtten = mainLight.shadowAttenuation;
-#endif
-}
+#ifndef X_CUSTOM_LIGHTING
+#define X_CUSTOM_LIGHTING
 
 #ifndef SHADERGRAPH_PREVIEW
 
@@ -50,7 +20,7 @@ Light GetAdditionalLightCustom(int pixelLightIndex, float3 worldPosition) {
 
 #endif
 
-void AddAdditionalLights_float(float Smoothness, float3 WorldPosition, float3 WorldNormal, float3 WorldView,
+void _AddAdditionalLights_float(float Smoothness, float3 WorldPosition, float3 WorldNormal, float3 WorldView,
     float MainDiffuse, float3 MainSpecular, float3 MainColor,
     out float Diffuse, out float3 Specular, out float3 Color) {
 
@@ -89,7 +59,7 @@ void AddAdditionalLights_float(float Smoothness, float3 WorldPosition, float3 Wo
 #endif
 }
 
-void AddAdditionalLights_half(half Smoothness, half3 WorldPosition, half3 WorldNormal, half3 WorldView,
+void _AddAdditionalLights_half(half Smoothness, half3 WorldPosition, half3 WorldNormal, half3 WorldView,
     half MainDiffuse, half3 MainSpecular, half3 MainColor,
     out half Diffuse, out half3 Specular, out half3 Color) {
 
@@ -129,70 +99,41 @@ void AddAdditionalLights_half(half Smoothness, half3 WorldPosition, half3 WorldN
 #endif
 }
 
-void AddAdditionalLightsSimple_float(float Smoothness, float3 WorldPosition, float3 WorldNormal, float3 WorldView,
-    float MainDiffuse, float3 MainSpecular, float3 MainColor,
-    out float Diffuse, out float3 Specular, out float3 Color) {
-
-    Diffuse = MainDiffuse;
-    Specular = MainSpecular;
-    Color = MainColor * (MainDiffuse + MainSpecular);
-
+void ExtLitModel_float(
+    float3 BaseColor, float3 DiffuseColor, float3 F0, float Smoothness,
+    float3 WorldPos, float3 WorldNormal, float3 WorldView,
+    float3 LightDir, float3 LightColor, float LightAtten,
+    out float3 Result)
+{
+    // TODO, make this PBR
+    float NDotL = saturate(dot(WorldNormal, LightDir));
+    float3 diffuse = NDotL * DiffuseColor;
+    Result = 0;
+    
 #ifndef SHADERGRAPH_PREVIEW
-
-    uint pixelLightCount = GetAdditionalLightsCount();
-
-#if USE_FORWARD_PLUS
-    // for Foward+ LIGHT_LOOP_BEGIN macro uses inputData.normalizedScreenSpaceUV and inputData.positionWS
-    InputData inputData = (InputData)0;
-    float4 screenPos = ComputeScreenPos(TransformWorldToHClip(WorldPosition));
-    inputData.normalizedScreenSpaceUV = screenPos.xy / screenPos.w;
-    inputData.positionWS = WorldPosition;
-#endif
-
-    LIGHT_LOOP_BEGIN(pixelLightCount)
-        Light light = GetAdditionalLightCustom(lightIndex, WorldPosition);
-        float NdotL = saturate(dot(WorldNormal, light.direction));
-        float atten = light.distanceAttenuation * light.shadowAttenuation;
-        float thisDiffuse = atten * NdotL;
-        Diffuse += thisDiffuse;
-        Color += light.color * thisDiffuse;
-    LIGHT_LOOP_END
-    float total = Diffuse;
-    Color = total <= 0 ? MainColor : Color / total;
+    // TODO, make this PBR
+    float3 specular = F0 * LightingSpecular(NDotL, LightDir, WorldNormal, WorldView, F0, Smoothness);
+    
+    Result = (diffuse + specular) * LightColor * LightAtten;
 #endif
 }
 
-void AddAdditionalLightsSimple_half(half Smoothness, half3 WorldPosition, half3 WorldNormal, half3 WorldView,
-    half MainDiffuse, half3 MainSpecular, half3 MainColor,
-    out half Diffuse, out half3 Specular, out half3 Color) {
-
-    Diffuse = MainDiffuse;
-    Specular = MainSpecular;
-    Color = MainColor * (MainDiffuse + MainSpecular);
-
+void ExtLitModel_half(
+    half3 BaseColor, half3 DiffuseColor, half3 F0, half Smoothness,
+    half3 WorldPos, half3 WorldNormal, half3 WorldView,
+    half3 LightDir, half3 LightColor, half LightAtten,
+    out half3 Result)
+{
+    // TODO, make this PBR
+    half NDotL = saturate(dot(WorldNormal, LightDir));
+    half3 diffuse = NDotL * DiffuseColor;
+    Result = 0;
+    
 #ifndef SHADERGRAPH_PREVIEW
-
-    uint pixelLightCount = GetAdditionalLightsCount();
-
-#if USE_FORWARD_PLUS
-    // for Foward+ LIGHT_LOOP_BEGIN macro uses inputData.normalizedScreenSpaceUV and inputData.positionWS
-    InputData inputData = (InputData)0;
-    float4 screenPos = ComputeScreenPos(TransformWorldToHClip(WorldPosition));
-    inputData.normalizedScreenSpaceUV = screenPos.xy / screenPos.w;
-    inputData.positionWS = WorldPosition;
-#endif
-
-    LIGHT_LOOP_BEGIN(pixelLightCount)
-        Light light = GetAdditionalLightCustom(lightIndex, WorldPosition);
-        half NdotL = saturate(dot(WorldNormal, light.direction));
-        half atten = light.distanceAttenuation * light.shadowAttenuation;
-        half thisDiffuse = atten * NdotL;
-        Diffuse += thisDiffuse;
-        Color += light.color * thisDiffuse;
-    LIGHT_LOOP_END
-    //needs to be float to avoid precision issues
-    float total = Diffuse;
-    Color = total <= 0 ? MainColor : Color / total;
+    // TODO, make this PBR
+    half3 specular = F0 * LightingSpecular(NDotL, LightDir, WorldNormal, WorldView, F0, Smoothness);
+    
+    Result = (diffuse + specular) * LightColor * LightAtten;
 #endif
 }
 
